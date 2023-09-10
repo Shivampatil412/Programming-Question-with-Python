2bc9c79a6f5ade908eeefcafc9cfafe4aad5ec02
shivampatil412 <shivampatil597@gmail.com>
2023-09-10 14:37:25 +0530

    Modified Reverse Number comment

--- reverseNumber.py
+++ reverseNumber.py
@@ -1,4 +1,17 @@
 #Python Program to Reverse a number
+'''
+121
+121 % 10 = 1
+0 * 10 + 1  = 1
+121 // 10 = 12
+
+12 % 10 = 2
+1 * 10 + 2  = 12
+12 // 10 = 1
+
+1 % 10 = 1
+12 * 10 + 1 = 121
+'''
 
 def reverseNum(num):
     sum = 0
